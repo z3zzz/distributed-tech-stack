e7f4b13226dd28de55601265805d015e5c7bf577
Kwangtae Kim <zabra7946@naver.com>
2022-10-16 14:18:51 +0900

    add axios for request

--- web/src/utils/request.js
+++ web/src/utils/request.js
@@ -1,3 +1,31 @@
-export default request = ({ method, uri }) => {
-  return 0;
-};
+import axios from "axios";
+
+async function get(uri) {
+  return axios.get(endpoint);
+}
+
+async function post(endpoint, data) {
+  const bodyData = JSON.stringify(data);
+
+  return axios.post(endpoint, bodyData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+async function patch(endpoint, data) {
+  const bodyData = JSON.stringify(data);
+
+  return axios.put(endpoint, bodyData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+async function del(endpoint) {
+  return axios.delete(endpoint);
+}
+
+export default request = ({ method, uri, body, language, db, server }) => {};
