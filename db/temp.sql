6b34ce136e50a2ed5926abdddf49182597750058
Kwangtae Kim <zabra7946@naver.com>
2022-10-19 06:29:21 +0000

    insert data

--- db/temp.sql
+++
@@ -1,6 +0,0 @@
-insert into portfolios (title, content, github_link, tech_stack) values (
-  'MySQL과 MongoDB의 Latency 비교 및 샤딩에 대한 연구',
-  'NoSQL과 RDBMS에 수백만 row의 데이터를 각각 삽입한 후, 텍스트 서치 API를 구현했을 때의  Latency를 비교, 샤딩의 필요성 연구',
-  'https://github.com/z3zzz/database-comparison',
-  ['MySQL', 'MongoDB', 'NodeJS', 'ReactJS', 'Nginx']
-)
